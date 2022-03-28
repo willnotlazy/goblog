c5b39ac9c4432f49462935d2897ccd7ba95e594c
willnotlazy <519478450@qq.com>
2022-03-28 16:28:11 +0800

    尝试ServerMux

--- main.go
+++ main.go
@@ -3,14 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>hello goblog</h1>")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "404 not found, path: "+r.URL.Path+", query: "+r.URL.RawQuery)
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":8088", nil)
+
+	route := http.NewServeMux()
+
+	route.HandleFunc("/", defaultHandler)
+	route.HandleFunc("/about", aboutHandler)
+
+	route.HandleFunc("/articles/", func(writer http.ResponseWriter, request *http.Request) {
+		id := strings.SplitN(request.URL.Path, "/", 3)[2]
+		fmt.Fprint(writer, "article id "+id)
+	})
+
+	http.ListenAndServe(":8088", route)
 }
