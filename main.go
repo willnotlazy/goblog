9567df6b164c85f802409d7fd375067bf0eb9c6c
willnotlazy <519478450@qq.com>
2022-04-06 15:17:46 +0800

    文章列表

--- main.go
+++ main.go
@@ -81,6 +81,16 @@ type Article struct {
 	ID    int64
 }
 
+func (article Article) Link() string {
+	showURL, err := router.Get("articles.show").URL("id", strconv.FormatInt(article.ID, 10))
+	if err != nil {
+		checkError(err)
+		return ""
+	}
+
+	return showURL.String()
+}
+
 func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 	id := getRouteVariable("id", r)
 
@@ -105,7 +115,27 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "访问文章列表")
+	rows, err := db.Query("SELECT * FROM articles")
+	checkError(err)
+	defer rows.Close()
+
+	var articles []Article
+
+	for rows.Next() {
+		var article Article
+		err := rows.Scan(&article.ID, &article.Title, &article.Body)
+		checkError(err)
+		articles = append(articles, article)
+	}
+
+	err = rows.Err()
+	checkError(err)
+
+	tmpl, err := template.ParseFiles("resources/views/articles/index.gohtml")
+	checkError(err)
+
+	err = tmpl.Execute(w, articles)
+	checkError(err)
 }
 
 type articlesFormData struct {
